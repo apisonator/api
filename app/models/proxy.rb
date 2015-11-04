35c480c272c36ba79b94614d0357695168168942
Jose Galisteo <ceritium@gmail.com>
2015-11-04 19:07:12 +0100

    validate format of subdomain

--- app/models/proxy.rb
+++ app/models/proxy.rb
@@ -2,7 +2,7 @@ class Proxy < ActiveRecord::Base
   belongs_to :user
   has_many :releases
 
-  validates :subdomain, uniqueness: true
+  validates :subdomain, uniqueness: true, format: { with: /\A\w[\w\-\/_]+\z/ }
   validates :endpoint, presence: true
 
   after_create :set_in_redis
